8e16d1ac8298358314b6b53ba435a4799d18e55d
nathan-assefa <nnathanassefa@gmail.com>
2023-03-01 00:12:20 -0800

    binary_tree

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,28 @@
+#include "binary_trees.h"
+
+/**
+ * tree_depth: returns the depth of a binary tree
+ * tree: the tree whose depth is to be returned
+ */
+size_t tree_depth(const binary_tree_t *tree)
+{
+    size_t a;
+
+    if (!tree)
+        return (0);
+
+    a = tree_depth(tree->parent);
+    return (a + 1);
+}
+
+/**
+ * binary_tree_depth: To find the depth of binary tree
+ * @tree: pointer to binary tree
+ */
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+    if (!tree)
+        return (0);
+
+    return (tree_depth(tree) - 1);
+}
