6e42d0b233f08fc0ee2da991c2d613f091a31b8e
nathan-assefa <nnathanassefa@gmail.com>
2023-03-01 11:40:46 -0800

    binary_tree

---
+++ 7-binary_tree_inorder.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+
+/**
+* binary_tree_postorder- Traversing via preorder
+* @tree: Tree to be traversed
+* @func: functional pointer
+*/
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (!tree || !func)
+		return;
+
+	binary_tree_preorder(tree->left, func);
+	binary_tree_preorder(tree->right, func);
+	func(tree->n);
+}
