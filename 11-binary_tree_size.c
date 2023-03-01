0cffadd6c77c84b9e23545078b5a051c94c69d53
nathan-assefa <nnathanassefa@gmail.com>
2023-03-01 00:52:54 -0800

    binary_tree

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_size - to find the size of the binary tree
+ * @tree: The tree whose size is to be solved
+ * Return: the size of a binary tree
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	if (tree) printf("tree->n: %d\n", tree->n);
+	size_t a, b;
+
+	if (!tree)
+		return (0);
+
+	a = binary_tree_size(tree->left);
+	b = binary_tree_size(tree->right);
+	return (a + b + 1);
+}
