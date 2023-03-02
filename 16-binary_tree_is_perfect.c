f5b4b1767236fd2929cdcc19e0fac83080a9c71e
nathan-assefa <nnathanassefa@gmail.com>
2023-03-02 04:59:46 -0800

    binary_tree

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,21 +1,59 @@
 #include "binary_trees.h"
 
 /**
-* binary_tree_is_perfect- check if BS is perfect BS
-* @tree: Tree to be chacked
-* Return: 1 on success and 0 on failure
-*/
-int binary_tree_is_perfect(const binary_tree_t *tree)
+ * _binary_tree_height - helper func for binary_tree_height
+ * @tree: input tree
+ * Return: height of tree
+ */
+size_t _binary_tree_height(const binary_tree_t *tree)
+{
+	size_t a, b;
+
+	if (!tree)
+		return (0);
+
+	a = _binary_tree_height(tree->left);
+	b = _binary_tree_height(tree->right);
+	return (MAX(a, b) + 1);
+}
+
+/**
+ * binary_tree_height - measures the height of a binary tree
+ * @tree: input tree
+ * Return: height of tree
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+	return (_binary_tree_height(tree) - 1);
+}
+
+/**
+ * binary_tree_size - measures the size of a binary tree
+ * @tree: input binary tree
+ * Return: number of descendant child nodes
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
 {
 	if (!tree)
 		return (0);
 
-	if (!tree->left && !tree->right)
-		return (1);
+	return (1 + binary_tree_size(tree->left) + binary_tree_size(tree->right));
+}
 
-	if (tree->left && tree->right)
-		return (binary_tree_is_perfect(tree->left->left)
-		&& binary_tree_is_perfect(tree->right->right));
+/**
+ * binary_tree_is_perfect - checks if a binary tree is perfect
+ * @tree: pointer to head of tree
+ * Return: 1 if true 0 if false
+ */
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+	int n, h;
 
-	return (0);
+	if (!tree)
+		return (0);
+	n = (int)binary_tree_size(tree);
+	h = binary_tree_height(tree);
+	return (n == (2 << h) - 1);
 }
