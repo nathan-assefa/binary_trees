242332a57ca76fcbbb9932c39535e842e4f5ccdb
nathan-assefa <nnathanassefa@gmail.com>
2023-03-04 01:57:36 -0800

    binary_tree

---
+++ 103-binary_tree_rotate_left.c
@@ -0,0 +1,25 @@
+#include "binary_trees.h"
+/**
+* binary_tree_rotate_left- Making right rotation
+* @tree: A binary tree
+* Return: rotated tree
+*/
+binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
+{
+	binary_tree_t *rotate;
+
+	rotate = tree->right;
+
+	if (!tree || !tree->right)
+		return (NULL);
+
+	if (rotate->left)
+		rotate->left->parent = tree;
+
+	tree->right = rotate->left;
+	rotate->left = tree;
+	rotate->parent = tree->parent;
+	tree->parent = rotate;
+
+	return (rotate);
+}
