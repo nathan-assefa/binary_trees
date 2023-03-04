efb6275eb0dbf30a9723ac28e7d6d4c2fa200a4c
nathan-assefa <nnathanassefa@gmail.com>
2023-03-04 02:15:31 -0800

    binary_tree

--- 103-binary_tree_rotate_left.c
+++ 103-binary_tree_rotate_left.c
@@ -9,15 +9,15 @@ binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
 {
 	binary_tree_t *rotate;
 
-	rotate = tree->right;
-	tree->right = rotate->left;
-
 	if (!tree || !tree->right)
 		return (NULL);
+
+	rotate = tree->right;
 
 	if (rotate->left)
 		rotate->left->parent = tree;
 
+	tree->right = rotate->left;
 	rotate->left = tree;
 	rotate->parent = tree->parent;
 	tree->parent = rotate;
